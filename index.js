a1b68df7e0640856d564c740dff493fe8646214e
Durlav Sahu <thedurlav@gmail.com>
2022-08-09 22:04:54 +0530

    first Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const { urlencoded } = require('express')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static("public"))
+app.get('/hello', (req, res) => res.send('Hello'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
